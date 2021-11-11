d32a315f8f733d8aad00de38891e65b983e48761
xh-dev <adamhung12@gmail.com>
2021-11-11 08:46:47 +0800

    Update and rename WithLogger.java to LoggerProvider.java

--- src/main/sources/dev/xethh/webtool/testRnrUtils/common/LoggerProvider.java
+++ src/main/sources/dev/xethh/webtool/testRnrUtils/common/LoggerProvider.java
@@ -3,7 +3,7 @@ package dev.xethh.webtool.testRnrUtils.common;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public interface LogProvider {
+public interface LoggerProvider {
     default Logger getLogger(){
         return LoggerFactory.getLogger(this.getClass());
     }
